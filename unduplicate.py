d61a0a4349f0281aa47e1ec5b10594c0a267bce4
Simon Brandhof <simon@brandhof.net>
2022-09-11 14:31:27 +0200

    Support multiple dirs as inputs

--- unduplicate.py
+++ unduplicate.py
@@ -19,26 +19,40 @@ def checksum(path):
     return h.hexdigest()
 
 
-if __name__ == '__main__':
-    root_path = sys.argv[1]
-    print(f"Analyzing {root_path}")
-    should_delete = len(sys.argv) == 3 and sys.argv[2] == "-delete"
+def configure_dirs():
+    result = []
+    for i, arg in enumerate(sys.argv):
+        if i == 0 or arg.startswith('-'):
+            continue
+        result.append(arg)
+    return result
+
+
+def should_delete():
+    for arg in sys.argv:
+        if arg == '-delete':
+           return True
+    return False
+
+
+def find_deletable_files(root_paths):
     data = {}
 
-    for root, dirs, files in os.walk(root_path, followlinks=False, topdown=True):
-        # Remove hidden files/dirs
-        # See https://stackoverflow.com/a/13454267/229031
-        files = [f for f in files if f[0] != '.']
-        dirs[:] = [d for d in dirs if d[0] != '.']
+    for root_path in root_paths:
+        for root, dirs, files in os.walk(root_path, followlinks=False, topdown=True):
+            # Remove hidden files/dirs
+            # See https://stackoverflow.com/a/13454267/229031
+            files = [f for f in files if f[0] != '.']
+            dirs[:] = [d for d in dirs if d[0] != '.']
 
-        for file in files:
-            file_path = os.path.join(root, file)
-            file_size = os.path.getsize(file_path)
-            if file_size == 0:
-                continue
-            files_of_same_size = data.get(file_size, [])
-            files_of_same_size.append(file_path)
-            data[file_size] = files_of_same_size
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_size = os.path.getsize(file_path)
+                if file_size == 0:
+                    continue
+                files_of_same_size = data.get(file_size, set())
+                files_of_same_size.add(file_path)
+                data[file_size] = files_of_same_size
 
     total_save = 0
     deletable_files = []
@@ -61,10 +75,12 @@ if __name__ == '__main__':
         for files_same_checksum in files_by_checksum.values():
             if len(files_same_checksum) <= 1:
                 continue
+
             duplicated_size = os.path.getsize(files_same_checksum[0]) * (len(files_same_checksum) - 1)
             total_save += duplicated_size
             print(f"{len(files_same_checksum)} duplicated files ({human_size(duplicated_size)} each):")
-            for i, f in enumerate(files_same_checksum):
+            # keep deterministic selection of files to be deletable by sorting the files
+            for i, f in enumerate(sorted(files_same_checksum)):
                 eligible = i > 0
                 wildcard = ''
                 if eligible:
@@ -73,10 +89,16 @@ if __name__ == '__main__':
                 print(f"    {f} {wildcard}")
             print('\n')
 
-    print(f"Analyzed {root_path}")
     print(f"Duplicated size: {human_size(total_save)}")
+    return deletable_files
 
-    if should_delete:
-        for file in deletable_files:
+
+if __name__ == '__main__':
+    root_dirs = configure_dirs()
+    print(f"Analyzing {root_dirs}")
+    _deletable_files = find_deletable_files(root_dirs)
+
+    if should_delete():
+        for file in _deletable_files:
             print(f"Deleting {file}")
-            os.remove(file)
+            #os.remove(file)
